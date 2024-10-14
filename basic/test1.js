c0ca69f1681b6b68f28ed9a70e094103177656fa
Rohit Das <141236628+rohitdasweaversweb@users.noreply.github.com>
2024-10-14 12:52:19 +0000

    variable

--- basic/test1.js
+++ basic/test1.js
@@ -1,4 +1,4 @@
-console.log("helo ");
+// console.log("helo ");
 const authourID=1236
 let authorEmail="r12@gmail.com"
 var authorname="Rohit Das"
@@ -6,5 +6,9 @@ var authorname="Rohit Das"
 authorEmail="assi23@gmail.com"
 
 let variableState;
-console.table([authourID,authorEmail,authorname,variableState]);
+// console.table([authourID,authorEmail,authorname,variableState]);
 // console.log(typeof authorname);
+console.log(typeof null)
+console.log(typeof undefined)
+
+
