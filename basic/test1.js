03248c217f0c2c882bf2db3304768fe403828866
Rohit Das <141236628+rohitdasweaversweb@users.noreply.github.com>
2024-10-14 12:48:18 +0000

    scope

---
+++ basic/test1.js
@@ -0,0 +1,10 @@
+console.log("helo ");
+const authourID=1236
+let authorEmail="r12@gmail.com"
+var authorname="Rohit Das"
+
+authorEmail="assi23@gmail.com"
+
+let variableState;
+console.table([authourID,authorEmail,authorname,variableState]);
+// console.log(typeof authorname);
