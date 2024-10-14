23476e0641715788af1cf3fa7da7570151d4398e
Rohit Das <141236628+rohitdasweaversweb@users.noreply.github.com>
2024-10-14 14:10:36 +0000

    dattype conversion

---
+++ basic/test2.js
@@ -0,0 +1,16 @@
+let score="rohit435345366"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber=Number(score);
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+let IsLoogedin=""
+let valueBollean=Boolean(IsLoogedin)
+console.log(valueBollean);
+console.log(typeof valueBollean);
+
+
+
