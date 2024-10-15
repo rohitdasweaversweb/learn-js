3cef3783aad260e65feea0592f513a831da1c3b8
Rohit Das <141236628+rohitdasweaversweb@users.noreply.github.com>
2024-10-15 08:15:17 +0000

    operations

--- basic/test2.js
+++ basic/test2.js
@@ -1,16 +1,23 @@
-let score="rohit435345366"
+let score=58
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueNumber=Number(score);
-console.log(typeof valueNumber);
-console.log(valueNumber);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
 
-let IsLoogedin=""
-let valueBollean=Boolean(IsLoogedin)
-console.log(valueBollean);
-console.log(typeof valueBollean);
+let IsLoogedin=33
+let valueBollean=String(IsLoogedin)
+// console.log(valueBollean);
+// console.log(typeof valueBollean);
+
+// console.log("1"+2);
+// console.log(1+9+"2");
 
 
+/****************operations*************/
+// console.log("2">1);
+// console.log(null>0);
+console.log(null== 0);
 
