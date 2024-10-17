5fff43f49acc9f472ac8f9155a8e4e57f83f2b60
Rohit Das <141236628+rohitdasweaversweb@users.noreply.github.com>
2024-10-17 13:23:42 +0000

    while loop

--- basic/test10.js
+++ basic/test10.js
@@ -1,9 +1,16 @@
-for (let i = 0; i <10; i++) {
-    const element = i;
-    // if(element== 6){
-    //     // console.log(`It Is A Best Number`);
-    //     continue;
-    // }
-    console.log(element);
+// for (let i = 0; i <10; i++) {
+//     const element = i;
+//     // if(element== 6){
+//     //     // console.log(`It Is A Best Number`);
+//     //     continue;
+//     // }
+//     console.log(element);
     
-}
+// }
+
+// const array=['Rohit','Arun','Tanmoy']
+// for (let index = 0; index < array.length-1 ;index++) {
+//     const element = array[index];
+//     console.log(element);
+// }
+
---
+++ basic/test11.js
@@ -0,0 +1,13 @@
+// const arr=['Red','green','Blue']
+// let myarr=0
+// while (myarr<arr.length) {
+//     console.log(`This the value of the array ${arr[myarr]}`);
+//     myarr=myarr+1
+// }
+
+
+let num=10
+while(num>0){
+    console.log(`the number is ${num}`);
+    num--;
+}
