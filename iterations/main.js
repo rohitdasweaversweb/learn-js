55450986e0a7b5abdf8351d4699359b152f0d578
Rohit Das <141236628+rohitdasweaversweb@users.noreply.github.com>
2025-01-02 11:22:49 +0000

    map functions

---
+++ iterations/main.js
@@ -0,0 +1,21 @@
+const myNum=[1,2,3,2,6,7];
+ for (const num of myNum) {
+  //  console.log(num);
+    
+ }
+
+
+ const mapex=new Map();
+ mapex.set('In',"INDIA");
+ mapex.set('us',"USA");
+ mapex.set('SW',"Switzerland");
+
+mapex.forEach((value,key)=>{
+    // console.log(`${key} -: ${value}`);
+    if(value=='INDIA'){
+        //console.log(`yaa hoooo`);
+        
+    }
+})
+//  console.log(mapex.size);
+ 
