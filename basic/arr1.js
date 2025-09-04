84a56e18cfbff825965251f2c261f41d1e68cdd0
Rohit Das <141236628+rohitdasweaversweb@users.noreply.github.com>
2025-09-04 07:28:56 +0000

    splice& slice

--- basic/arr1.js
+++ basic/arr1.js
@@ -2,5 +2,12 @@ const numbers = [1, 2, 3, 4, 5]
 // console.log(numbers.length);
 // numbers.push(6)
 // numbers.pop()
-numbers.unshift()
-console.log(numbers)    
+// numbers.unshift(25)
+// numbers.shift()
+const num2=numbers.slice(1,3)
+console.log("B",num2)    
+
+const num3=numbers.splice(1,1,"Graps");
+console.log("C",num3)
+console.log(typeof numbers);
+
